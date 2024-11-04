5e43406d3f7276bae1393a2bbc434d00de3f06c8
MattyxD22 <mathiaschristensen2009@live.dk>
2024-11-04 10:27:10 +0100

    fixed functionality

--- bachelorfe/src/app/components/full-calendar/full-calendar.component.ts
+++ bachelorfe/src/app/components/full-calendar/full-calendar.component.ts
@@ -18,19 +18,14 @@ interface CalendarEvent {
   standalone: true,
   imports: [FullCalendarModule],
   templateUrl: './full-calendar.component.html',
-  styleUrls: ['./full-calendar.component.scss']
+  styleUrls: ['./full-calendar.component.scss'],
 })
-
 export class FullCalendarComponent {
   calendarVisible = signal(true);
   currentEvents = signal<CalendarEvent[]>([]);
 
-  constructor(private eventService: EventService) {
-    this.visArbejdstimer();
-  }
-
   teamupStore = inject(TeamupStore);
-  clickupStore = inject(ClickupStore)
+  clickupStore = inject(ClickupStore);
 
   // Get events from the store
   readonly events = this.teamupStore.getUserCalendar;
@@ -38,16 +33,12 @@ export class FullCalendarComponent {
 
   // FullCalendar options
   calendarOptions = signal<CalendarOptions>({
-    plugins: [
-      interactionPlugin,
-      dayGridPlugin,
-      timeGridPlugin
-    ],
+    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin],
     locale: 'da',
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      right: 'dayGridMonth,timeGridWeek,timeGridDay',
     },
     initialView: 'timeGridWeek',
     weekends: true,
@@ -77,41 +68,52 @@ export class FullCalendarComponent {
     eventContent: (arg) => {
       const { event } = arg;
       const { extendedProps } = event;
-  
+
       // Example: Display task details if available
       const title = event.title || 'Event';
       const taskDetails = extendedProps['taskDetails'];
-      
+
       // You can customize the display as needed
       return {
         html: `
           <div class="border-b">
             <strong>${title}</strong>
             ${taskDetails ? `<div>${taskDetails.title}</div>` : ''}
-            ${taskDetails ? `<div>${taskDetails.duration.hours}h ${taskDetails.duration.minutes}m</div>` : ''}
+            ${
+              taskDetails
+                ? `<div>${taskDetails.duration.hours}h ${taskDetails.duration.minutes}m</div>`
+                : ''
+            }
           </div>
-        `
+        `,
       };
-    }
+    },
   });
 
   private updateCalendarOptions() {
     this.calendarOptions.set({
       ...this.calendarOptions(),
-      events: this.currentEvents(), 
+      events: this.currentEvents(),
     });
   }
 
   visArbejdstimer() {
-    this.currentEvents.set(this.eventService.getArbejdstimerEvents());
-    this.updateCalendarOptions(); 
-    console.log('Arbejdstimer events:', this.currentEvents());
+    const currentEvents = this.events();
+    this.updateCalendarEvents(currentEvents, false);
+    // this.currentEvents.set(this.eventService.getArbejdstimerEvents());
+    // this.updateCalendarOptions();
+    // console.log('Arbejdstimer events:', this.currentEvents());
   }
 
   visFridage() {
-    this.currentEvents.set(this.eventService.getFridageEvents());
-    this.updateCalendarOptions();
-    console.log('Fridage events:', this.currentEvents());
+
+
+    const currentEvents = this.events();
+    this.updateCalendarEvents(currentEvents, true);
+
+    // this.currentEvents.set(this.eventService.getFridageEvents());
+    // this.updateCalendarOptions();
+    // console.log('Fridage events:', this.currentEvents());
   }
 
   get options() {
@@ -119,27 +121,38 @@ export class FullCalendarComponent {
   }
 
   // Use `effect` to update calendar events when `events` changes
-  private eventsEffect = effect(() => {
-    const currentEvents = this.events();
-    this.updateCalendarEvents(currentEvents);
-  }, { allowSignalWrites: true });
+  private eventsEffect = effect(
+    () => {
+      const currentEvents = this.events();
+      this.updateCalendarEvents(currentEvents, false);
+    },
+    { allowSignalWrites: true }
+  );
 
   // Method to update calendar events
-  private updateCalendarEvents(events: any[]) {
-    this.calendarOptions.set({ events: this.transformEvents(events) });
+  private updateCalendarEvents(events: any[], showSickDays: boolean) {
+    this.calendarOptions.set({ events: this.transformEvents(events, showSickDays) });
   }
 
   // Method to transform the event data
-  private transformEvents(events: any[]): any[] {
+  private transformEvents(events: any[], showSickDays: boolean): any[] {
     const transformedEvents: any[] = [];
 
-    const allowedCalendarIds = this.teamupStore.subcalendars()
+    let allowedCalendarIds = this.teamupStore
+      .subcalendars()
       .filter((item: any) => item.name === 'Office' || item.name === 'Remote')
       .map((calendar: any) => calendar.id); // Extract only the ids
 
+    if (showSickDays) {
+      allowedCalendarIds = this.teamupStore
+        .subcalendars()
+        .filter((item: any) => item.name === 'Sick' || item.name === 'Holiday')
+        .map((calendar: any) => calendar.id); // Extract only the ids
+    }
+
     const clickupTasks = this.clickupStore.tasks();
 
-    events.forEach(event => {
+    events.forEach((event) => {
       if (allowedCalendarIds.includes(event.subcalendar_id)) {
         const eventStartDate = new Date(event.start_dt).toDateString(); // Gets a string like "Fri Nov 01 2024"
 
@@ -148,7 +161,7 @@ export class FullCalendarComponent {
           // Convert the dateLogged from string to a number and then to a Date object
           const taskDate = new Date(parseInt(task.dateLogged)); // Ensure we're converting to a number
           const taskStartDate = taskDate.toDateString(); // Convert to date string for comparison
-    
+
           // Return true if the dates match
           return taskStartDate === eventStartDate;
         });
@@ -161,19 +174,20 @@ export class FullCalendarComponent {
           allDay: event.all_day || false,
           extendedProps: {
             email: event.custom?.email || '',
-            taskDetails: correspondingTask ? {
-              title: correspondingTask.taskTitle,
-              dateLogged: correspondingTask.dateLogged,
-              loggedBy: correspondingTask.loggedBy,
-              duration: correspondingTask.duration // or any other property you want to include
-            } : null // Set to null if no corresponding task is found
-          }
+            taskDetails: correspondingTask
+              ? {
+                  title: correspondingTask.taskTitle,
+                  dateLogged: correspondingTask.dateLogged,
+                  loggedBy: correspondingTask.loggedBy,
+                  duration: correspondingTask.duration, // or any other property you want to include
+                }
+              : null, // Set to null if no corresponding task is found
+          },
         });
       }
     });
 
     console.log(transformedEvents);
-
 
     return transformedEvents;
   }
